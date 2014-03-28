2ed87f41e8c16f2981516965e7d0b5f7b16e0243
Jaskaran Rana <jaskaran@jaskaran-linux.(none)>
2014-03-28 23:31:45 +0530

    updated settings.py

--- TaskMan/settings.py
+++ TaskMan/settings.py
@@ -98,3 +98,16 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
+TEMPLATE_DIRS = (
+    root('templates'),
+)
