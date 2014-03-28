b1bd5fda27ebed638123b5a6b77175d37041eeaa
Jaskaran Rana <jaskaran@jaskaran-linux.(none)>
2014-03-28 23:36:26 +0530

    updated settings.py

--- TaskMan/settings.py
+++ TaskMan/settings.py
@@ -81,8 +81,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
-
 AUTH_USER_MODEL = 'taskmanager.MyUser'
 
 DATABASES['default'] =  dj_database_url.config()
