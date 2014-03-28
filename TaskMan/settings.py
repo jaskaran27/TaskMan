c54d9a72cba767ec97c6d11e0fc6d1dfec20bada
Jaskaran Rana <jaskaran@jaskaran-linux.(none)>
2014-03-28 23:19:48 +0530

    updated settings.py

--- TaskMan/settings.py
+++ TaskMan/settings.py
@@ -55,8 +55,12 @@ WSGI_APPLICATION = 'TaskMan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'task_db',                      # Or path to database file if using sqlite3.
+        'USER': 'postgres',                      # Not used with sqlite3.
+        'PASSWORD': 'root',                  # Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
